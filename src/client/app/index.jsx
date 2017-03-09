df6f751740f409a86fe5838658a8c0e0bbed9e09
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-03-09 13:10:57 +0000

    Added Exercises to complete in relevant area of code

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -13,6 +13,9 @@ function Square (props) {
   
 }
 
+/* 1. Display the move locations in the format "(1, 3)" instead of "6". */
+
+/* 3. Rewrite Board to use two loops to make the squares instead of hardcoding them. */
 class Board extends React.Component {
   constructor(props) {
     super();
@@ -77,9 +80,13 @@ class Game extends React.Component {
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) ? false : true,
+      xIsNext: (step % 2) ? false : true
     });
   }
+
+  /* 2. Bold the currently-selected item in the move list. */
+  /* 4. Add a toggle button that lets you sort the moves in either ascending or descending order. */
+  /* 5. When someone wins, highlight the three squares that caused the win. */
 
   render() {
     const history = this.state.history;
